e4eb68b83cf3ebbe48f79fdac05d83a8fabf7fea
miguel <miguel.hurtado@onewayecommerce.com>
2022-10-13 19:53:39 -0500

    new changue and finish

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-$(document).ready(function() {
-	$(window).scroll(function(){
-		if(this.scrollY > 20 ){
-			$('.navbar').addClass('sticky');
-		}else{
-			$('.navbar').removeClass('sticky');
-		}
-	});
-})
+$(document).ready(function () {
+	$(window).scroll(function () {
+		
+	  if (this.scrollY > 20) {
+		$(".navbar").addClass("sticky");
+	  } else {
+		$(".navbar").removeClass("sticky");
+	  }
+
+	  if (this.scrollY > 500) {
+		$(".scroll-up-btn").addClass("show");
+	  } else {
+		$(".scroll-up-btn").removeClass("show");
+	  }
+}});
+
+alert("si funciona");
